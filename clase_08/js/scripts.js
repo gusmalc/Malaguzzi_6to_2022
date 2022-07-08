cde320398b49e3f5ea8a534b88bb001249c35083
gusmalc <gustavo@malcotti.com>
2022-07-07 23:47:58 -0300

    1

---
+++ clase_08/js/scripts.js
@@ -0,0 +1,33 @@
+//let uno = 4;
+
+// uno = "hola";
+// uno = 2;
+// uno = "2";
+// uno = false; // true
+
+// let num1 = 3;
+// let num2 = 2;
+
+// let resultado = num1 + num2;
+
+// num1 = num1 + 1;
+
+//num1--;
+
+//alert(uno);
+
+//console.log(num1);
+
+
+let edad;
+let respuesta;
+
+edad = 25;
+
+if(edad < 18){
+    respuesta = "Sos menor de edad";
+}else{
+    respuesta = "Sos mayor de edad";
+}
+
+console.log(respuesta);
